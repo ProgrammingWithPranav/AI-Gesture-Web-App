593a6d91340c4ba5440290a69ce90930e2f1b9e1
Prana Tantri <67408817+ProgrammingWithPranav@users.noreply.github.com>
2021-04-17 15:47:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-function preload() {
-    // Preload
+function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 550);
+
+  canvas = createCanvas(550, 550);
+  canvas.position(700, 200);
+  pose = ml5.poseNet(video, modelLoaded);
+
+  pose.on("pose", gotResults);
 }
 
-function setup() {
-    // Setup
+function gotResults(results) {
+    if(results.length > 0) {
+        console.log(results);
+    }
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
 }
 
 function draw() {
-    // Draw
+  background(255, 0, 0);
 }
