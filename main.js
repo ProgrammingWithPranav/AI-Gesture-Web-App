da902413eae52e9e197a186379d07e7b883a662c
Pranav Tantri <67408817+ProgrammingWithPranav@users.noreply.github.com>
2021-05-13 19:33:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,9 +9,25 @@ function setup() {
   pose.on("pose", gotResults);
 }
 
+var noseX = 0;
+var noseY = 0;
+var rightWristX = 0;
+var leftWristX = 0;
+var difference = 0;
+
 function gotResults(results) {
     if(results.length > 0) {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X = " + noseX + "nose Y = " + noseY)
+
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + "rightWristX " + rightWristX + "Difference " + difference)
+
     }
 }
 
@@ -21,4 +37,8 @@ function modelLoaded() {
 
 function draw() {
   background(255, 0, 0);
+  document.getElementById("w&h").innerHTML = difference + "px";
+  fill(0, 255, 0);
+  stroke(0, 255 ,0);
+  square(noseX, noseY, difference);
 }
